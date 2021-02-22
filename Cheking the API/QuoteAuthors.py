179e41657ff6eb6cf1f2ce44d7d46dcfdb90fe53
AnaAGG <a.garciagar5@gmail.com>
2021-02-22 20:55:31 +0100

    deletions

--- Cheking the API/QuoteAuthors.py
+++
@@ -1,8 +0,0 @@
-from mongo import insert_data, read_data, update_data, delete_data
-from check import check_author, check_parameters
-from bson import ObjectId
-
-
-def complete_list():
-    res = read_data({}, {"Author":1, "Quote":1, "Category":1, "_id":0}, "Quote_Author" )
-    return res
